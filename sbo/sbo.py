e46ca3b49c40cc374d8318e751d49ef401623763
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-03-02 18:37:21 +0000

    Adding doc strings

--- sbo/sbo.py
+++ sbo/sbo.py
@@ -1,7 +1,7 @@
 """
 This is an implimentation of the structured bayesian optimisation approach based on
-BOAT: Building Auto-Tuners with Structured Bayesian Optimization
-Valentin Dalibard, Michael Schaarschmidt, Eiko Yoneki
+BOAT: Building Auto-Tuners with Structured Bayesian Optimization by Dalibard et al.
+and https://pyro.ai/examples/bo.html.
 
 """
 
@@ -22,8 +22,8 @@ from pyro.nn import PyroSample, PyroModule
 from pyro.infer import autoguide, SVI, Trace_ELBO
 
 
-normal_phi = lambda x: torch.exp(-x.pow(2) / 2) / np.sqrt(2 * np.pi)
-normal_Phi = lambda x: (1 + torch.erf(x / np.sqrt(2))) / 2
+NORMAL_PHI_1 = lambda x: torch.exp(-x.pow(2) / 2) / np.sqrt(2 * np.pi)
+NORMAL_PHI_2 = lambda x: (1 + torch.erf(x / np.sqrt(2))) / 2
 
 
 class TargetFunction:
@@ -53,8 +53,25 @@ class TargetFunction:
 
 
 class SemiParametricModel(PyroModule):
+    """
+    Semi-parametric model class which extends PyroModule and employs Gaussian process
+        regression model (GPRegression).
+    """
+
     def __init__(self, X, y, parametric_mean, kernel, noise=None, jitter=1e-06):
-        """ Defines a semi-parametric model, where the `parametric_mean` is a `PyroModule` """
+        """ 
+        Defines a semi-parametric model.
+
+        Args:
+            X (torch.Tensor) – A input data for training. Its first dimension is the number of data points.
+            y (torch.Tensor) – An output data for training. Its last dimension is the number of data points.
+            parametric_mean (callable) - A parametric mean function implemneted as PyroModule
+            kernel (Kernel) – A Pyro kernel object, which is the covariance function 𝑘.
+            noise (torch.Tensor) – Variance of Gaussian noise of this model.
+            jitter (float) – A small positive term which is added into the diagonal 
+                part of a covariance matrix to help stablize its Cholesky decomposition.
+        """
+
         super().__init__()
 
         self.X = X
@@ -67,12 +84,40 @@ class SemiParametricModel(PyroModule):
 
     @pyro.nn.pyro_method
     def model(self):
+        """
+        A model stochastic function, where we explicitly subtract out the parametric mean
+            from the training output data. This means that we model based on the difference
+            (errors) between ParametricMeanFn and the observations (TargetFunction).
+
+        Args:
+            X (torch.Tensor) – A input data for training. Its first dimension is the number of data points.
+            y (torch.Tensor) – An output data for training. Its last dimension is the number of data points.
+            parametric_mean (callable) - A parametric mean function implemneted as PyroModule
+            kernel (Kernel) – A Pyro kernel object, which is the covariance function 𝑘.
+            noise (torch.Tensor) – Variance of Gaussian noise of this model.
+            jitter (float) – A small positive term which is added into the diagonal 
+                part of a covariance matrix to help stablize its Cholesky decomposition.
+        Returns:
+            A “model” stochastic function for a GP. 
+        """
+
         # Model definition code: explicitly subtract out the parametric mean, then call gp.model
         self.gp.set_data(self.X, self.y - self.parametric_mean(self.X))
+        
         return self.gp.model()
 
     def forward(self, X):
-        """ Predict on new data points """
+        """
+        Computes the mean and covariance matrix (or variance) of Gaussian Process posterior on the
+            difference data. Then the obtained mean value together with the parametric mean
+            function are used to sample values for $y$, which in turn are used to sample the
+            expected improvement values.
+
+        Args:
+            X (torch.Tensor) – A input data for training. Its first dimension is the number of data points.
+        Returns:
+            y_hat (float) - The mean, in case we want to ignore the GP noise for some reason later
+        """
 
         # reset "data" of GP to reflect mean estimate
         self.gp.set_data(self.X, self.y - self.parametric_mean(self.X))
@@ -87,21 +132,34 @@ class SemiParametricModel(PyroModule):
         # compute expected improvement
         y_min = self.y.min()
         delta = y_min - y_hat
+        
         EI = (
             delta.clamp_min(0.0)
-            + sigma * normal_phi(delta / sigma)
-            - delta.abs() * normal_Phi(delta / sigma)
+            + sigma * NORMAL_PHI_1(delta / sigma)
+            - delta.abs() * NORMAL_PHI_2(delta / sigma)
         )
 
+        # sample the expected improvement
         pyro.sample("EI", dist.Delta(-EI))
 
-        # return the mean, in case we want to ignore the GP noise for some reason later
         return y_hat
 
 
 class GPRegressionModule(PyroModule):
+    """
+    A model class which extends PyroModule and employs Gaussian process
+        regression model (GPRegression).
+    """
+
     def __init__(self, X, y, kernel):
-        """ Defines a PyroModule which wraps GPRegression """
+        """ Defines a PyroModule which wraps GPRegression 
+
+         Args:
+            X (torch.Tensor) – A input data for training. Its first dimension is the number of data points.
+            y (torch.Tensor) – An output data for training. Its last dimension is the number of data points.
+            kernel (Kernel) – A Pyro kernel object, which is the covariance function 𝑘.
+        """
+
         super().__init__()
         self.X = X
         self.y = y
@@ -110,10 +168,27 @@ class GPRegressionModule(PyroModule):
 
     @pyro.nn.pyro_method
     def model(self):
+        """
+        A model stochastic function.
+
+        Returns:
+            A “model” stochastic function for a GP. 
+        """
+
         return self.gp.model()
 
     def forward(self, X):
-        """ Predict on new data points """
+         """
+        Computes the mean and covariance matrix (or variance) of Gaussian Process posterior. Then the 
+            obtained mean value is used to sample values for $y$, which in turn are used to sample the
+            expected improvement values.
+
+        Args:
+            X (torch.Tensor) – A input data for training. Its first dimension is the number of data points.
+        Returns:
+            mu (float) - The mean, in case we want to ignore the GP noise for some reason later
+        """
+
         # sample mu, sigma
         mu, sigma = self.gp(X)
 
@@ -125,18 +200,28 @@ class GPRegressionModule(PyroModule):
         delta = y_min - mu
         EI = (
             delta.clamp_min(0.0)
-            + sigma * normal_phi(delta / sigma)
-            - delta.abs() * normal_Phi(delta / sigma)
+            + sigma * NORMAL_PHI_1(delta / sigma)
+            - delta.abs() * NORMAL_PHI_2(delta / sigma)
         )
 
         pyro.sample("EI", dist.Delta(-EI))
 
-        # return the mean, in case we want to ignore the GP noise for some reason later
         return mu
 
 
 def train(model, optimizer, loss, num_steps=1000):
-    """ Trains a model. """
+    """ 
+    Trains a model. 
+    
+    Args:
+        model (PyroModule instance) – the model
+        optimizer (pyro.optim.PyroOptim) – a wrapper for a PyTorch optimizer.
+        loss (pyro.infer.elbo.ELBO) – an instance of a subclass of ELBO.
+        num_steps (int (1000)) - the number of optimization steps.
+    Returns:
+        guide (pyro.infer.autoguide.guides.AutoContinuous) - a MultivariateNormal posterior distribution.
+        losses (list(floats)) - a list of losses during the training procedure
+    """
 
     # Autoguide
     guide = autoguide.AutoMultivariateNormal(model.model)
@@ -152,7 +237,18 @@ def train(model, optimizer, loss, num_steps=1000):
 
 
 def transf_values(values, constr, dims, inv_mode=False):
-    """ Transforming (un)constrained variables to (un)constrained domain """
+    """ 
+    Transforms constrained values to unconstrained domain and vice versa.
+    
+    Args:
+        values (torch.Tensor) - values to be transformed.
+        constr (list) - a list of constrains.
+        dims (int) - a number of dimensions.
+        inv_mode (bool) - a flag for the direction of transformation:
+            False: constrained->unconstrained, True: constrained->unconstrained.
+    Returns:
+        x (torch.Tensor) - transformed values.
+    """
 
     x_tmp = ()
     for i in range(dims):
@@ -166,16 +262,23 @@ def transf_values(values, constr, dims, inv_mode=False):
     return x
 
 
-def find_a_candidate(
-    model_predict,
-    return_site,
-    optimizer,
-    x_init,
-    constr,
-    num_steps=1000,
-    num_samples=5,
-    lr=0.1):
-    """ Finds new candidate """
+def find_a_candidate(model_predict, return_site, optimizer, x_init, constr,
+                     num_steps=1000, lr=0.1):
+    
+    """ 
+    Finds a new candidate point.
+    
+    Args:
+        model_predict (pyro.infer.Predictive) - predictive distribution.
+        return_site (list, tuple, or set) – sites to return.
+        optimizer (torch.optim) - acquisition function optimizer.
+        x_init (torch.Tensor) - initial optiisation point.
+        constr (list) - list of constrains.
+        num_steps (int (1000)) - number of acquisition function optimization steps.
+        lr (float (0.1)) - learning rate for acquisition function optimization.
+    Returns:
+        x (torch.Tensor) - a new candidate point.
+    """
 
     x_dims = x_init.shape[-1]
 
@@ -201,17 +304,22 @@ def find_a_candidate(
     return x.detach()
 
 
-def next_x(
-    model_predict,
-    return_site,
-    optimizer,
-    target,
-    num_steps=1000,
-    num_candidates=5,
-    num_samples=5,
-    lr=0.1,
-):
-    """ Finds the next best candidate on the acquisition function surface """
+def next_x(model_predict, return_site, optimizer, target,
+           num_steps=1000, num_candidates=5, lr=0.1):
+    """ 
+    Finds the next best candidate on the acquisition function surface.
+    
+    Args:
+        model_predict (pyro.infer.Predictive) - predictive distribution.
+        return_site (list, tuple, or set) – sites to return.
+        optimizer (torch.optim) - acquisition function optimizer.
+        target (TargetFunction) - the target (black-box) function.
+        num_steps (int (1000)) - number of acquisition function optimization steps.
+        num_candidates (int (5)) - number of candidates to try during each optimisation step.
+        lr (float (0.1)) - learning rate for acquisition function optimization.
+    Returns:
+        (torch.Tensor) - a new candidate point.
+    """
 
     candidates = []
     values = []
@@ -229,8 +337,7 @@ def next_x(
             x_init,
             target.contstraints,
             num_steps=num_steps,
-            num_samples=num_samples,
-            lr=lr,
+            lr=lr
         )
 
         y = model_predict(x_can)[return_site].mean(0)
@@ -266,16 +373,21 @@ def next_x(
         return candidates[0]
 
 
-def update_posterior(model, optimizer, loss, obj_function, x_new, num_steps=1000):
+def update_posterior(model, optimizer, loss, target, x_new, num_steps=1000):
     """
-    Updates
-        model:
-        optimizer:
-        loss:
-        obj_function: objectivefunction
-        x_new: a new value of x
-        num_steps:
+    Updates the postrior distribution.
 
+    Args:
+        model (PyroModule instance) – the model.
+        optimizer (pyro.optim.PyroOptim) – a wrapper for a PyTorch optimizer.
+        loss (pyro.infer.elbo.ELBO) – an instance of a subclass of ELBO.
+        target (TargetFunction) - the target (black-box) function.
+        x_new  (torch.Tensor) - a candidate point.
+        num_steps (int (1000)) - the number of optimization steps.
+
+    Returns:
+        guide (pyro.infer.autoguide.guides.AutoContinuous) - an updated MultivariateNormal posterior distribution.
+        losses (list(floats)) - a list of losses during the training procedure.
     """
 
     # evaluate f at new point
@@ -288,7 +400,6 @@ def update_posterior(model, optimizer, loss, obj_function, x_new, num_steps=1000
     model.X = X
     model.y = y
 
-    # TODO: Check if this necessary
     model.gp.set_data(X, y)
 
     guide, losses = train(model, optimizer, loss, num_steps=num_steps)
@@ -296,27 +407,33 @@ def update_posterior(model, optimizer, loss, obj_function, x_new, num_steps=1000
     return guide, losses
 
 
-def step(
-    model,
-    guide,
-    optimizer,
-    loss,
-    target,
-    acqf_optimizer,
-    opti_num_steps=1000,
-    acqf_opti_num_steps=1000,
-    acqf_opti_lr=0.1,
-    num_samples=1,
-    num_candidates=5,
-    return_site="EI",
-):
+def step(model, guide, optimizer, loss, target, acqf_optimizer,
+         opti_num_steps=1000, acqf_opti_num_steps=1000,
+         acqf_opti_lr=0.1, num_samples=1, num_candidates=5,
+         return_site="EI"):
     """
-    Performs a bayesian optimisation step.
+    Performs a bayesian optimisation step. This involves generating a predictive model, 
+    which is then used to find a new value of x at which the target function needs to 
+    be evaluated, and updating the model after the new x is obtained.
 
-    This involves generating a predictive model, which is then used to find a new value of x at
-    which the target function needs to be evaluated, and updating the model after the new
-    x is obtained.
+    Args:
+        model (PyroModule instance) – the model
+        guide (pyro.infer.autoguide.guides.AutoContinuous) - a MultivariateNormal posterior distribution.
+        optimizer (pyro.optim.PyroOptim) – a wrapper for a PyTorch optimizer.
+        loss (pyro.infer.elbo.ELBO) – an instance of a subclass of ELBO.
+        target (TargetFunction) - the target (black-box) function
+        acqf_optimizer (torch.optim) - the acquisition function optimizer.
+        opti_num_step (int (1000)) - the number of inference optimization steps.
+        acqf_opti_num_steps (int (1000)) - the number of acquisition function optimization steps.
+        acqf_opti_lr (float (0.1)) - the learning rate for acquisition function optimization.
+        num_samples (int (1)) - number of samples to draw from the predictive distribution.
+        num_candidates (int (5)) - number of candidates to try during each optimisation step.
+        return_site (list, tuple, or set ("EI")) – sites to return.
 
+    Returns:
+        new_guide (pyro.infer.autoguide.guides.AutoContinuous) - an updated MultivariateNormal posterior distribution.
+        predict (pyro.infer.Predictive) - a predictive distribution.
+        losses (list(floats)) - a list of losses during the training procedure.
     """
 
     if guide is not None:
@@ -327,7 +444,7 @@ def step(
             copy.copy(model),
             guide=guide,
             num_samples=num_samples,
-            return_sites=("y", return_site),
+            return_sites=("y", return_site)
         )
 
         # Finds the next candidate
@@ -338,8 +455,7 @@ def step(
             target,
             num_steps=acqf_opti_num_steps,
             num_candidates=num_candidates,
-            num_samples=num_samples,
-            lr=acqf_opti_lr,
+            lr=acqf_opti_lr
         )
 
         # Updates posterior
